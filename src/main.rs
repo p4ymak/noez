736b819e4598ad882fd7782b6ea98e910341af15
Роман Чумак <chumak.r@granit-concern.ru>
2025-09-28 15:09:21 +0300

    enter to stop

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,23 @@
+mod stereo;
+
 use rodio::{SampleRate, Sink, source::Pink};
 use std::{thread, time::Duration};
-
-mod stereo;
 use stereo::Stereo;
+
 const RATE: SampleRate = 48000;
 const STEREO_SPREAD: f32 = 0.7;
 
 fn main() {
-    let audio =
-        rodio::OutputStreamBuilder::open_default_stream().expect("open default audio stream");
-    let sink = Sink::connect_new(audio.mixer());
-    let stereo = Stereo::new(Pink::new(RATE), Pink::new(RATE), STEREO_SPREAD);
-    sink.append(stereo);
+    std::thread::spawn(|| {
+        let audio =
+            rodio::OutputStreamBuilder::open_default_stream().expect("open default audio stream");
+        let sink = Sink::connect_new(audio.mixer());
+        let stereo = Stereo::new(Pink::new(RATE), Pink::new(RATE), STEREO_SPREAD);
+        sink.append(stereo);
+        thread::sleep(Duration::MAX);
+    });
 
-    println!("\x1b[105mEnjoy Pink Noise in Stereo\x1b[0m");
-    thread::sleep(Duration::MAX);
+    println!("\x1b[105mEnjoy Pink Noise in Stereo..\x1b[0m Press ENTER to Stop.");
+
+    std::io::stdin().read_line(&mut String::new()).ok();
 }
